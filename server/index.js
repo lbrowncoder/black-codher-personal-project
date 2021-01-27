04916e82ee3c1b1dc09b419e5fb79466b38e9f84
marab <69154212+lbrowncoder@users.noreply.github.com>
2021-01-27 15:22:43 +0000

    infodatabase

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-require("dotenv").config();
 require("./models/nurseryInfo");
+require("dotenv").config();
 
 const app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
  process.env.MONGODB_URI ||
-  `mongodb+srv://lbrown:Theloft0607@mern.wiwpv.mongodb.net/info?retryWrites=true&w=majority`,
+  `mongodb+srv://lbrown:Theloft0607@mern.wiwpv.mongodb.net/black-codher-personal-project?retryWrites=true&w=majority`,
  {
   useNewUrlParser: true,
   useUnifiedTopology: true,
